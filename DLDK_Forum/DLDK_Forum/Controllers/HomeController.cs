69521062c468ef599bc8b65e08002c1f4803398d
Duongtsnc1 <duongbui.hlnm@gmail.com>
2020-10-06 15:31:23 +0700

    tao repo

---
+++ DLDK_Forum/DLDK_Forum/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DLDK_Forum.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Login_Logout()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult Account()
+        {
+            return View();
+        }
+
+    }
+}
